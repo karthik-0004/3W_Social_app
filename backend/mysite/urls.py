32192c1545cb262ee50aaa6a227f7b124f08dc80
karthik-0004 <karthikgangaji@gmail.com>
2026-04-02 20:37:05 +0530

    checking 1

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -17,11 +17,18 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path, re_path
 from django.views.static import serve
 from rest_framework_simplejwt.views import TokenRefreshView
 
+
+def health_check(_request):
+    return JsonResponse({'status': 'ok'})
+
 urlpatterns = [
+    path('', health_check),
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
